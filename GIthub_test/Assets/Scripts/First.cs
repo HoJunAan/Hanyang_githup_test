14e716372a4cde59b2e733d8880773155611d24c
WildLawAan <anan7940@hanyang.ac.kr>
2022-11-01 02:52:13 +0900

    firstcommit
    firstcommit

---
+++ GIthub_test/Assets/Scripts/First.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class First : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello_World!");
+    }
+
+    // Update is called once per frame
+   
+}
